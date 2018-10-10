36a79d0f4ddf92617f024eb0dbd01f477bef0592
Nathan Holzworth <nholzwor@uncc.edu>
2018-10-10 11:39:17 -0400

    -exercise - ClickedPoint

--- ClickedPoints.js
+++ ClickedPoints.js
@@ -56,15 +56,16 @@ function click(ev, gl, canvas, a_Position) {
   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
   // Store the coordinates to g_points array
-  g_points.push(x); g_points.push(y);
+  g_points.push([x,y]);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   var len = g_points.length;
-  for(var i = 0; i < len; i += 2) {
+  for(var i = 0; i < len; i++) {
+    var xy = g_points[i];
     // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
 
     // Draw
     gl.drawArrays(gl.POINTS, 0, 1);
